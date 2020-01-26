25322955d7b5adb86d25255ace287dfa964c5b4d
Matas <matas.jarilinas@stud.vgtu.lt>
2020-01-26 22:04:28 +0200

    Added gh-pages for live webPage

---
+++ build/precache-manifest.959d74637d1ced7c06946276be569372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3405ae65a0f900ea4fb5bbef5b9fd8",
+    "url": "/blackJack/index.html"
+  },
+  {
+    "revision": "d794e7d1e536dde33d54",
+    "url": "/blackJack/static/css/main.2be8da40.chunk.css"
+  },
+  {
+    "revision": "a50c9d0ad42e34eaa98b",
+    "url": "/blackJack/static/js/2.ec36fa94.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/blackJack/static/js/2.ec36fa94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d794e7d1e536dde33d54",
+    "url": "/blackJack/static/js/main.9c207ad8.chunk.js"
+  },
+  {
+    "revision": "1691633eea667e26eaf4",
+    "url": "/blackJack/static/js/runtime-main.45d88674.js"
+  }
+]);
