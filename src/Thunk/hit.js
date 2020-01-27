9cc2a36c89691c1e7a84f3a9c71f3e3f4144798a
Matas <matas.jarilinas@stud.vgtu.lt>
2020-01-27 11:25:19 +0200

    small fix

--- src/Thunk/hit.js
+++ src/Thunk/hit.js
@@ -4,12 +4,13 @@ import { stand } from "./stand";
 
 export const hit = () => (dispatch, getState) => {
   const copyDeck = [...getState().gameData.deck];
-  const random = Math.floor(Math.random() * copyDeck.length);
+  const random = Math.floor(Math.random() * (copyDeck.length - 1));
   copyDeck.splice(random, 1);
   dispatch(
     setPlayerDeck(getState().gameData.playerDeck.concat(copyDeck[random]))
   );
   dispatch(setDeck(copyDeck));
+
   if (calculatePoints(getState().gameData.playerDeck) > 21) {
     dispatch(stand());
   }
